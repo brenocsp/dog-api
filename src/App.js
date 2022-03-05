ad7de2c88ad9922bb62f2d2d8820c6b083e8f1b7
Breno Pimenta <brenocsp@gmail.com>
2022-03-05 16:53:56 -0300

    creating header component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
+import React from 'react';
 import axios from 'axios';
-import './App.css';
-import api from "./services/api";
 
-axios.defaults.baseURL = 'https://api.thedogapi.com/v1/images/search'
-axios.defaults.headers.common['x-api-key'] = '4be46b5c-8453-462a-8a28-1f73e711fb62'
+import Header from "./components/Header.js";
+import DogImages from "./components/DogImages.js"
+
+import './App.css';
+
+// axios.defaults.baseURL = 'https://api.thedogapi.com/v1/images/search'
+// axios.defaults.headers.common['x-api-key'] = '4be46b5c-8453-462a-8a28-1f73e711fb62'
 
 function App() {
   return (
-    <div className="App">
-      <header className="Dog API">
-      </header>
+    <div>
+      <Header></Header>
+      <h2 className='subtitle'>Um site para quem ama cachorrinhos!</h2>
+      <DogImages></DogImages>
     </div>
   );
 }
---
+++ src/components/DogImages.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function DogImages() {
+  return (
+    <div>
+        <p className='mostrar-mais'>Mostrar Mais</p>
+        <button>
+            <img src="assets/arrowDown.png" alt="Botão para mostrar mais fotos de cachorros" />
+        </button>
+    </div>
+  );
+}
+
+export default DogImages;
