4711afb8d41a69886b0284a62185083f1e05793d
Breno Pimenta <brenocsp@gmail.com>
2022-03-05 19:38:30 -0300

    dog api get method

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React from 'react';
-import axios from 'axios';
-
 import Header from "./components/Header.js";
 import DogImages from "./components/DogImages.js"
 
 import './App.css';
 
-// axios.defaults.baseURL = 'https://api.thedogapi.com/v1/images/search'
-// axios.defaults.headers.common['x-api-key'] = '4be46b5c-8453-462a-8a28-1f73e711fb62'
-
 function App() {
   return (
     <div>
-      <Header></Header>
+      <Header/>
       <h2 className='subtitle'>Um site para quem ama cachorrinhos!</h2>
-      <DogImages></DogImages>
+      <DogImages/>
+      <h2 className='subtitle'>Fim da Pag</h2>
     </div>
   );
 }
--- src/components/DogImages.js
+++ src/components/DogImages.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React from "react";
+import axios from 'axios';
 
 function DogImages() {
-  return (
+
+  for (let i=0; i<2; i++) {
+    axios.get('https://api.thedogapi.com/v1/images/search', {
+      method: 'GET',
+      headers: {  
+        'x-api-key': '2d7f1333-8853-4e28-a48c-04209802b01d'
+      }
+    }).then(resposta => carregaElementosNaPagina(resposta.data))
+      .catch(err=>console.log(err));
+  }
+
+  function carregaElementosNaPagina(dogData) {
+    const img = document.createElement("img");
+    img.src = dogData[0].url;
+    document.body.appendChild(img);
+    
+    const resultado = document.querySelector('.resultado');
+    resultado.appendChild(img);
+  }
+
+  return ( 
     <div>
+        <div className="resultado">
+          
+        </div>
         <p className='mostrar-mais'>Mostrar Mais</p>
         <button>
-            <img src="assets/arrowDown.png" alt="Botão para mostrar mais fotos de cachorros" />
+            <img src="assets/arrowDown.png" alt="BotÃ£o para mostrar mais fotos de cachorros" />
         </button>
     </div>
   );
